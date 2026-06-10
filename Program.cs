2676714ef3bc1b9af7300c312f8f75422ec32e04
sal <whatever@example.com>
2026-06-10 20:56:30 +0200

    Initial commit - Lox interpreter in C#

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace firstCompilerProject;
+    
+public class Program
+{
+    // cslox interpreter implementation
+    // Implementation following craftinginterpreters.com from @Robert Nystrom
+
+    internal static bool HadError { get; set; }
+
+    static int Main(string[] args)
+    {
+        try
+        {
+           if (args.Length > 1)
+           {
+               Console.WriteLine("Usage: cslox [script]");
+               return 64; // UsageError FreeBSD sysexits
+           }
+           else if (args.Length == 1)
+           {
+               RunFile(args[1]);
+           }
+           else
+           {
+               RunPrompt();
+           }
+        }
+        catch (IOException)
+        {
+            return 74; //IOException FreeBSD sysexits
+        }
+        return 0;
+    }
+
+    private static void RunFile(string path)
+    {
+        Run(File.ReadAllText(path, Encoding.UTF8));
+    }
+
+    private static void RunPrompt()
+    {
+        while (true)
+        {
+            string? line;
+            line = Console.ReadLine();
+            if (line == null)
+                break;
+            Run(line);
+        }
+    }
+
+    private static void Run(string source)
+    {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.ScanTokens();
+
+        foreach (Token token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    }
+
+    internal static void Error(int line, string message)
+    {
+        Report(line, "", message);
+    }
+
+    private static void Report(int line, string where, string message)
+    {
+        Console.Error.WriteLine("[line " +line+ "] Error" + where + ": " + message);
+        HadError = true;
+    }
+}
---
+++ Token.cs
@@ -0,0 +1,22 @@
+namespace firstCompilerProject;
+
+class Token
+{
+   readonly TokenType type;
+   readonly string lexeme;
+   readonly object? literal;
+   readonly int line;
+
+   internal Token(TokenType type, string lexeme, object? literal, int line)
+   {
+       this.type = type;
+       this.lexeme = lexeme;
+       this.literal = literal;
+       this.line = line;
+   }
+
+   public override string ToString()
+   {
+       return type + " " + lexeme + " " + literal;
+   }
+}
